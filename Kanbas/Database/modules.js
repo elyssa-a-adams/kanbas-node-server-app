664a93592530fca81dbc5fe5ff08c287d64ee748
elyssa-a-adams <elyssaa1995@gmail.com>
2024-04-02 22:55:27 -0400

    first commit

---
+++ Kanbas/Database/modules.js
@@ -0,0 +1,158 @@
+export default [
+    {
+      "_id": "DOG101",
+      "name": "How to Be A Dog",
+      "description": "Basic principles of Being a Dog.",
+      "course": "DOG101",
+      "lessons": [
+        {
+          "_id": "L101",
+          "name": "History of Barking",
+          "description": "A brief history of woofing.",
+          "module": "M101"
+        },
+        {
+          "_id": "L102",
+          "name": "Barking Fundamentals",
+          "description": "Basic principles of how to bark.",
+          "module": "M101"
+        },
+        {
+          "_id": "L103",
+          "name": "Barking Types",
+          "description": "Difference between barking and yipping.",
+          "module": "M101"
+        }
+      ]
+    },
+    {
+      "_id": "M102",
+      "name": "How to Be A Good Pet",
+      "description": "Understanding how to get more treats.",
+      "course": "DOG101",
+      "lessons": [
+        {
+          "_id": "L201",
+          "name": "Treat Types",
+          "description": "Overview of different types of treats.",
+          "module": "M102"
+        },
+        {
+          "_id": "L202",
+          "name": "Walking Processes",
+          "description": "Understanding how to go on walks and get more treats.",
+          "module": "M102"
+        },
+        {
+          "_id": "L203",
+          "name": "Treat Instability",
+          "description": "Understanding treat instability and mitigation.",
+          "module": "M102"
+        }
+      ]
+    },
+    {
+      "_id": "M201",
+      "name": "Fundamentals of Swimming",
+      "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+      "course": "SWIM102",
+      "lessons": [
+        {
+          "_id": "M202",
+          "name": "Water Flow",
+          "description": "Understanding water behaviors.",
+          "module": "SWIM102"
+        },
+        {
+          "_id": "M203",
+          "name": "Swimming Basics",
+          "description": "Study of the basics of swimming.",
+          "module": "SWIM102"
+        },
+        {
+          "_id": "M301",
+          "name": "Swimming Structural Design",
+          "description": "Fundamentals of designing swimming structures and floaties selection.",
+          "module": "GD103"
+        }
+      ]
+    },
+    {
+      "_id": "M302",
+      "name": "Swimming Mechanics",
+      "description": "Understanding swimming dynamics and mission planning.",
+      "course": "SWIM102",
+      "lessons": [
+        {
+          "_id": "M202",
+          "name": "Pools",
+          "description": "Understanding swimming in pools.",
+          "module": "SWIM102"
+        },
+        {
+          "_id": "M203",
+          "name": "Oceans",
+          "description": "Understanding swimming in oceans.",
+          "module": "SWIM102"
+        },
+        {
+          "_id": "M301",
+          "name": "Lake",
+          "description": "Understanding swimming in lakes.",
+          "module": "GD103"
+        }
+      ]
+    },
+    {
+      "_id": "M201",
+      "name": "Fundamentals of Swimming",
+      "description": "Basic aerodynamic concepts and fluid dynamics principles.",
+      "course": "SWIM102",
+      "lessons": [
+        {
+          "_id": "M202",
+          "name": "Water Flow",
+          "description": "Understanding water behaviors.",
+          "module": "SWIM102"
+        },
+        {
+          "_id": "M203",
+          "name": "Swimming Basics",
+          "description": "Study of the basics of swimming.",
+          "module": "SWIM102"
+        },
+        {
+          "_id": "M301",
+          "name": "Swimming Structural Design",
+          "description": "Fundamentals of designing swimming structures and floaties selection.",
+          "module": "GD103"
+        }
+      ]
+    },
+    {
+      "_id": "M302",
+      "name": "Gardening Mechanics",
+      "description": "Understanding gardening dynamics and garden planning.",
+      "course": "GD103",
+      "lessons": [
+        {
+          "_id": "M202",
+          "name": "Flowers",
+          "description": "Understanding the types of flowers to garden.",
+          "module": "GD102"
+        },
+        {
+          "_id": "M203",
+          "name": "Vegetable Garden",
+          "description": "Growing your own tomatoes.",
+          "module": "GD102"
+        },
+        {
+          "_id": "M301",
+          "name": "Hydroponics",
+          "description": "Growing plants in water.",
+          "module": "GD102"
+        }
+      ]
+    }
+  ]
